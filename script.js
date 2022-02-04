38fc3770b5c010dcc783c1b186cb5ad5b2b1b0b9
Helly Shah <68399409+hellyshah11@users.noreply.github.com>
2022-02-04 09:28:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
     responsive:{
-        0:{
+        500:{
             items:1
         },
-        600:{
-            items:3
+        800:{
+            items:2,
+            margin: 10
         },
         1000:{
             items:3
         }
     }
+})
+var owl = $('.owl-carousel');
+owl.owlCarousel();
+$('.r-btn').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+$('.l-btn').click(function() {
+    owl.trigger('prev.owl.carousel', [300]);
 })
