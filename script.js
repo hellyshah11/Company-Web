9b2675a1a747a618b9b771df57037988b6558340
Jaydeep Ravat <47079969+Jaydeep45@users.noreply.github.com>
2022-02-10 13:26:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:10,
+    margin:20,
+    items: 4,
     responsive:{
         500:{
             items:1
         },
         800:{
             items:2,
-            margin: 10
+            margin: 5
         },
         1000:{
-            items:3
+            items:3,
+            margin:10
         }
     }
 })
@@ -22,3 +24,9 @@ $('.r-btn').click(function() {
 $('.l-btn').click(function() {
     owl.trigger('prev.owl.carousel', [300]);
 })
+
+
+var txt = document.querySelector('.inner-text p');
+txt.innerHTML = txt.innerText.split('').map(
+    (char, i) => `<span style="transform:rotate(${i*45}deg)">${char}</span>`
+).join("")
